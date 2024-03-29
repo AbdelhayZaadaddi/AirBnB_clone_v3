528176d23df4476d5e488a0225f1d94dc9cf5b6c
Abdelhay Zaadaddi <abdezaadaddi06@gmail.com>
2024-03-29 14:17:08 +0000

    fixing buggs

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
-from flask import Flask, Blueprint, jsonify
+from flask import jsonify
 from api.v1.views import app_views
 
-@app_views.route('/status', methods=['GET'])
+
+@app_views.route('/status')
 def get_status():
+    """ Returns the status of the API """
     return jsonify({"status": "OK"})

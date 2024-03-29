6c6bfe50db3b167dcf1c704cda6d6e239b2f9fe8
Abdelhay Zaadaddi <abdezaadaddi06@gmail.com>
2024-03-29 13:43:20 +0000

    create the new unit test

--- tests/test_models/test_engine/test_db_storage.py
+++ tests/test_models/test_engine/test_db_storage.py
@@ -66,6 +66,26 @@ test_db_storage.py'])
                              "{:s} method needs a docstring".format(func[0]))
             self.assertTrue(len(func[1].__doc__) >= 1,
                             "{:s} method needs a docstring".format(func[0]))
+    
+    def test_get(self):
+        """Test get method"""
+        new_state = State(name="Marrakech")
+        self.storage.new(new_state)
+        key = "State.{}".format(new_state.id)
+        result = self.storage.get("State", new_state.id)
+        self.assertTrue(result.id, new_state.id)
+        self.assertIsInstance(result, State)
+
+    def test_count(self):
+        """Test count method"""
+        old_count = self.storage.count("State")
+        new_state1 = State(name="NewYork")
+        self.storage.new(new_state1)
+        new_state2 = State(name="Virginia")
+        self.storage.new(new_state2)
+        new_state3 = State(name="California")
+        self.storage.new(new_state3)
+        self.assertEqual(old_count + 3, self.storage.count("State"))
 
 
 class TestFileStorage(unittest.TestCase):

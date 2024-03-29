a6c5134de0a3f3c160a6f9a50695d2f13377bc57
Abdelhay Zaadaddi <abdezaadaddi06@gmail.com>
2024-03-29 13:32:37 +0000

    ll

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -78,3 +78,10 @@ class DBStorage:
     def get(self, cls, id):
         """retrieve one object from the database by class and id"""
         return self.all(cls).get(cls.__name__ + '.' + id)
+    
+    def count(self, cls=None):
+        """count the number of objects in storage"""
+        if cls:
+            return len(self.all(cls))
+        else:
+            return sum(len(self.all(cls)) for cls in classes.values())

5cfb2dd984f2604682084b7c4556efc5520b4381
Abdelhay Zaadaddi <abdezaadaddi06@gmail.com>
2024-03-29 14:13:41 +0000

    fixing bugggs

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,19 +4,19 @@
 from flask import Flask
 from api.v1.views import app_views
 from models import storage
-import os
+from os import getenv
 
 app = Flask(__name__)
 
-app.register_blueprint(app_views, url_prefix="/api/v1")
+app.register_blueprint(app_views)
 
 @app.teardown_appcontext
-def teardown_appcontext(exception):
+def teardown_appcontext(exception=None):
     """Closes the storage"""
     storage.close()
 
 
 if __name__ == "__main__":
-    host = os.getenv("HBNB_API_HOST", '0.0.0.0')
-    port = int(os.getenv("HBNB_API_PORT", 5000))
-    app.run(host=host, port=port, threaded=True, debug=True)
+    HBNB_HOST = getenv("HBNB_API_HOST", "0.0.0.0")
+    HBNB_PORT = getenv("HBNB_API_PORT", 5000)
+    app.run(host=HBNB_HOST, port=HBNB_PORT, debug=True, threaded=True)
